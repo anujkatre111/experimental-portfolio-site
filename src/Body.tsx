6d2d36447cb227e8eefda8af425e399d1ba2451c
Anuj Katre <anujkatre72@gmail.com>
2026-02-13 21:25:51 +0530

    new changes

--- src/Body.tsx
+++ src/Body.tsx
@@ -1,16 +1,18 @@
 
 import Navbar from './components/Navbar'
+import MusicPlayer from './components/MusicPlayer'
 import { Outlet } from 'react-router-dom'
 
 const Body = () => {
   return (
     <div>
-      <div className='bg-white w-screen h-screen mx-auto flex justify-center'>
-    <div className='w-[500px] flex flex-col'>
-        <Navbar/>
-        <Outlet/>
+      <MusicPlayer />
+      <div className='bg-white w-screen min-h-screen mx-auto flex justify-center px-4 sm:px-6 md:px-8'>
+        <div className='w-full max-w-[500px] flex flex-col'>
+          <Navbar/>
+          <Outlet/>
+        </div>
       </div>
-    </div>
     </div>
   )
 }
